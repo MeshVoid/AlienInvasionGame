7788856a3531e98cb2010ab1b9d16b98da47bcb4
MeshVoid <meshvoid@gmail.com>
2022-08-01 13:12:44 +0600

    game, player, settings updates, movement and screen collision now working.

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,6 +2,8 @@ import sys
 import pygame
 
 from settings import Settings
+from player import PlayerShip
+
 
 class AlienInvasionGame:
     """Overall class to manage game assets and behavior."""
@@ -10,12 +12,12 @@ class AlienInvasionGame:
         """Initialize the game, and create game resources."""
         pygame.init()
         self.settings = Settings()
-
-        self.screen = pygame.display.set_mode(
-            (self.settings.screen_width,
-             self.settings.screen_height))
+        #self._set_full_screen_mode()
+        self._set_windowed_mode()
 
         pygame.display.set_caption('Alien Invasion Game')
+
+        self.player = PlayerShip(self)
 
         # Set the background color.
         self.bg_color = (0, 0, 120)
@@ -23,17 +25,59 @@ class AlienInvasionGame:
     def run_game(self):
         """Start the main loop for the game."""
         while True:
-            # Watch for keyboard and mouse events.
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
+            self._check_events()
+            self._update_screen()
+            self.player.update()
 
-            # Redraw the screen during each pass throug the loop.
-            self.screen.fill(self.settings.bg_color)
+    def _check_events(self):
+        """Check for keypresses and mouse events."""
+        # Watch for keyboard and mouse events.
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                self._check_keydown_events(event)
+            elif event.type == pygame.KEYUP:
+                self._check_keyup_events(event)
 
-            # Make the most recently drawn screen visible.
-            pygame.display.flip()
+    def _check_keydown_events(self, event):
+        """Respond to keypresses."""
+        if event.key == pygame.K_RIGHT:
+            self.player.moving_right = True
+        elif event.key == pygame.K_LEFT:
+            self.player.moving_left = True
+        # press q to quit
+        elif event.key == pygame.K_ESCAPE:
+            sys.exit()
 
+    def _check_keyup_events(self, event):
+        """Respond to key releases."""
+        if event.key == pygame.K_RIGHT:
+            self.player.moving_right = False
+        elif event.key == pygame.K_LEFT:
+            self.player.moving_left = False
+
+    def _update_screen(self):
+        """Update images on the screen, and flip to the new screen."""
+
+        # Redraw the screen during each pass throug the loop.
+        self.screen.fill(self.settings.bg_color)
+        self.player.blitme()
+        # Make the most recently drawn screen visible.
+        pygame.display.flip()
+
+    def _set_full_screen_mode(self):
+        """Run the game in a fullscreen mode"""
+        self.screen = pygame.display.set_mode(
+            (0, 0), pygame.FULLSCREEN)
+        self.settings.screen_width = self.screen.get_rect().width
+        self.settings.screen_height = self.screen.get_rect().height
+
+    def _set_windowed_mode(self):
+        """Run the game in a window"""
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_width,
+             self.settings.screen_height))
 
 
 if __name__ == "__main__":
--- settings.py
+++ settings.py
@@ -3,6 +3,10 @@ class Settings:
 
     def __init__(self):
         """Initialize the game's settings."""
+        # Screen settings
         self.screen_width = 800
         self.screen_height = 600
         self.bg_color = (0, 0, 120)
+
+        # Ship settings
+        self.ship_speed = 1.1
